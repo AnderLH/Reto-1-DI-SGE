e0bc99eae34ba9fe622826e1ca6d52a15f5596c0
AnderLH <ander.lopezdevallejohi@elorrieta-errekamari.com>
2023-10-19 17:55:50 +0000

    department migration

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\DepartamentController;
+Route::controller(DepartamentController::class)->group(function () {
+Route::get('/departaments', 'index')->name('departaments.index');
+Route::get('/departaments/{post}', 'show')->name('departaments.show');
+});
