388c13f1fa5e8d5313aa5db67c1dadaf8e321272
AnderLH <ander.lopezdevallejohi@elorrieta-errekamari.com>
2023-11-10 19:56:13 +0000

    more data

--- database/seeders/DataSeeder.php
+++ database/seeders/DataSeeder.php
@@ -28,9 +28,9 @@ class DataSeeder extends Seeder
         ]);
 
         DB::table("priorities")->insert([
-            ["name"=> "priorities1","created_at"=> now(),],
-            ["name"=> "priorities2","created_at"=> now(),],
-            ["name"=> "priorities3","created_at"=> now(),],
+            ["name"=> "High Priority","created_at"=> now(),],
+            ["name"=> "Medium Priority","created_at"=> now(),],
+            ["name"=> "Low Priority","created_at"=> now(),],
         ]);
 
         DB::table('statuses')->insert([
@@ -40,36 +40,106 @@ class DataSeeder extends Seeder
         ]);
 
         DB::table('users')->insert([[
-            "name"=> "user1",
-            "email"=>"user1@elorrieta.com",
+            "name"=> "ander",
+            "email"=>"ander@elorrieta.com",
             "email_verified_at"=> now(),
-            "password"=>"12345678",
+            "password"=>bcrypt('12345678'),
             "departament_id"=>"1",
             'created_at'=> now(),
         ],
         [
-            "name"=> "user2",
-            "email"=>"user2@elorrieta.com",
+            "name"=> "baichun",
+            "email"=>"baichun@elorrieta.com",
             "email_verified_at"=> now(),
-            "password"=>"147258369",
+            "password"=>bcrypt('147258369'),
             "departament_id"=>"1",
+            'created_at'=> now(),
+        ],
+        [
+            "name"=> "guest",
+            "email"=>"guest@elorrieta.com",
+            "email_verified_at"=> now(),
+            "password"=>bcrypt('963852741'),
+            "departament_id"=>"2",
             'created_at'=> now(),
         ]
             
         ]);
 
         DB::table('incidents')->insert([
-            ["title" => "Bug pagina web","text"=>"No crea un usuario en el register","user_id" => "1","departament_id" => "1","status"=>"1","priority"=>"2","category"=>"1","minutes"=>"30",'created_at'=> now(),],
-            ["title" => "Problema con la función de búsqueda", "text" => "La función de búsqueda no encuentra resultados","user_id" => "2", "departament_id" => "3", "status" => "1", "priority" => "2", "category" => "2", "minutes" => "60", 'created_at' => now(),],
-            ["title" => "Error en la página de inicio", "text" => "La página de inicio no carga correctamente","user_id" => "1", "departament_id" => "2", "status" => "2", "priority" => "3", "category" => "1", "minutes" => "45", 'created_at' => now(),],
-            ["title" => "Error en la página de contacto", "text" => "El formulario de contacto no envía correos electrónicos","user_id" => "2", "departament_id" => "1", "status" => "1", "priority" => "1", "category" => "3", "minutes" => "20", 'created_at' => now(),],
-        ]);
-
-        DB::table('coments') ->insert([
-            "incident_id"=>"1",
-            "coment"=>"comentario1",
-            "created_at"=> now(),
+            ["title" => "Bug pagina web","text"=>"No crea un usuario en el register","user_id" => "1","departament_id" => "1","status_id"=>"1","priority_id"=>"2","category_id"=>"1","minutes"=>"30",'created_at'=> now(),],
+            ["title" => "Problema con la función de búsqueda", "text" => "La función de búsqueda no encuentra resultados","user_id" => "2", "departament_id" => "3", "status_id" => "1", "priority_id" => "2", "category_id" => "2", "minutes" => "60", 'created_at' => now(),],
+            ["title" => "Error en la página de inicio", "text" => "La página de inicio no carga correctamente","user_id" => "1", "departament_id" => "2", "status_id" => "2", "priority_id" => "3", "category_id" => "1", "minutes" => "45", 'created_at' => now(),],
+            ["title" => "Error en la página de contacto", "text" => "El formulario de contacto no envía correos electrónicos","user_id" => "2", "departament_id" => "1", "status_id" => "1", "priority_id" => "1", "category_id" => "3", "minutes" => "20", 'created_at' => now(),],
+            [
+                "title" => "Problema con la carrito de compras",
+                "text" => "El carrito de compras no guarda los productos seleccionados",
+                "user_id" => "2",
+                "departament_id" => "4",
+                "status_id" => "1",
+                "priority_id" => "2",
+                "category_id" => "2",
+                "minutes" => "40",
+                "created_at" => now(),
+            ],
+            [
+                "title" => "Error en la funcionalidad de chat en vivo",
+                "text" => "Los mensajes del chat en vivo no se envían correctamente",
+                "user_id" => "2",
+                "departament_id" => "2",
+                "status_id" => "1",
+                "priority_id" => "2",
+                "category_id" => "2",
+                "minutes" => "55",
+                "created_at" => now(),
+            ],
+            [
+                "title" => "Problema de seguridad en la aplicación móvil",
+                "text" => "La aplicación móvil muestra datos sensibles sin autenticación",
+                "user_id" => "1",
+                "departament_id" => "3",
+                "status_id" => "2",
+                "priority_id" => "3",
+                "category_id" => "1",
+                "minutes" => "70",
+                "created_at" => now(),
+            ],
+            [
+                "title" => "Error en la generación de informes",
+                "text" => "La generación de informes falla con grandes conjuntos de datos",
+                "user_id" => "2",
+                "departament_id" => "4",
+                "status_id" => "1",
+                "priority_id" => "1",
+                "category_id" => "2",
+                "minutes" => "25",
+                "created_at" => now(),
+            ]
             
         ]);
+
+        DB::table('comments') ->insert([
+            [
+                "incident_id"=>"1",
+                "text"=>"comentario1",
+                "created_at"=> now(),
+                "user_id" => "1",
+            ],
+            [
+                "incident_id"=>"2",
+                "text"=>"comentario2",
+                "created_at"=> now(),
+                "user_id" => "1",
+            ],
+            [
+                "incident_id"=>"3",
+                "text"=>"comentario3",
+                "created_at"=> now(),
+                "user_id" => "1",
+            ]
+            
+        ]);
+
+        
     }
 }
